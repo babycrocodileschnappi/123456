625bece3a288e295a18f483f7e3f010f741a979e
cuijiawei <3086764005@qq.com>
2023-08-16 16:09:29 +0800

    update DetailPage

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { fetchPokemonById } from '../components/api';
 import '../tailwind.css';
-import {getPokemonBackgroundColor } from '../components/pokemonUtils';
+import { getPokemonBackgroundColor } from '../components/pokemonUtils';
 
 const DetailPage: React.FC = () => {
     const { id } = useParams<{ id?: string }>();
@@ -19,65 +19,82 @@ const DetailPage: React.FC = () => {
 
     return (
         <div className={`flex flex-col items-center pd-4`}>
-            {pokemon && (
-                <div className={`flex flex-col items-center ${getPokemonBackgroundColor(pokemon.types[0].type.name)} rounded-lg shadow-md h-screen w-screen pd-4`}>
-                    <img src={pokemon.sprites.other['official-artwork'].front_default} alt={pokemon.name} width={475} height={475} className="mb-2" />
-                    <h2 className="text-2xl font-bold m-4">Name: {pokemon.name}</h2>
-                    <div className="flex flex-row space-x-4">
-                        <div className="bg-gray-100 rounded-xl shadow-lg p-2 my-2 px-4">
-                            <p>Type: {pokemon.types[0].type.name}</p>
-                        </div>
-                        <div className="bg-black rounded-xl shadow-lg p-2 my-2 px-4">
-                            <p className="text-white">Height: {pokemon.height / 10} m</p>
-                        </div>
-                        <div className="bg-gray-100 rounded-xl shadow-lg p-2 my-2 px-4">
-                            <p>Weight: {pokemon.weight / 10} kg</p>
-                        </div>
-                        <div className="bg-black rounded-xl shadow-lg p-2 my-2 px-4">
-                            <p className="text-white">Base experience: {pokemon.base_experience} </p>
-                        </div>
-                        <div className="bg-gray-100 rounded-xl shadow-lg p-2 my-2 px-4">
-                            <p>Order: {pokemon.order} </p>
-                        </div>
-                    </div>
-
-
-                    <div className="flex flex-row space-x-4">
-                        <div className="bg-black rounded-xl shadow-lg p-2 my-2 px-4">
-                            <h3 className="text-lg text-white font-semibold mb-2">Abilities:</h3>
-                            <ul>
-                                {pokemon.abilities.map((ability: any) => (
-                                    <li className="text-white" key={ability.ability.name}>{ability.ability.name}</li>
-                                ))}
-                            </ul>
-                        </div>
-
-
-                        <div className="bg-gray-100 rounded-xl shadow-lg p-2 my-2 px-4">
-                            <h3 className="text-lg font-semibold mb-2">Move :</h3>
-                            <ul>
-                                {pokemon.moves.slice(0, 4).map((move: any) => (
-                                    <li key={move.move.id}>{move.move.name}</li>
-                                ))}
-                            </ul>
-                        </div>
-
-                        <div className="bg-black rounded-xl shadow-lg p-2 my-2 px-4">
-                            <h3 className="text-lg text-white font-semibold mb-2">Stat :</h3>
-                            <ul>
-                                {pokemon.stats.slice(0, 4).map((stat: any) => (
-                                    <li className="text-white" key={stat.stat.id}>{stat.stat.name}</li>
-                                ))}
-                            </ul>
-                        </div>
-
-                    </div>
-
-                    <Link to="/" className="text-blue-500 hover:underline">Back to HomePage</Link>
-                </div>
-            )}
+            <PokemonDetails pokemon={pokemon} />
+            <Link to="/" className="text-blue-500 hover:underline">Back to HomePage</Link>
         </div>
     );
 };
 
 export default DetailPage;
+
+const pokemonInfoBox = "rounded-xl shadow-lg p-2 my-2 px-4";
+
+const PokemonDetails: React.FC<{ pokemon: any }> = ({ pokemon }) => {
+    if (!pokemon) {
+        return null; // or render a loading indicator
+    }
+    const { types, sprites, name, height, weight, base_experience, order, abilities, moves, stats } = pokemon;
+
+    return (
+        <div className={`flex flex-col items-center ${getPokemonBackgroundColor(types[0].type.name)} rounded-lg shadow-md h-screen w-screen pd-4`}>
+            <img src={sprites.other['official-artwork'].front_default} alt={name} width={475} height={475} className="mb-2" />
+            <h2 className="text-2xl font-bold m-4">Name: {name}</h2>
+            <div className="flex flex-row space-x-4">
+                <div className={`bg-gray-100 ${pokemonInfoBox}`}>
+                    <p>Type: {types[0].type.name}</p>
+                </div>
+                <div className={`bg-black ${pokemonInfoBox}`}>
+                    <p className="text-white">Height: {height / 10} m</p>
+                </div>
+                <div className={`bg-gray-100 ${pokemonInfoBox}`}>
+                    <p>Weight: {weight / 10} kg</p>
+                </div>
+                <div className={`bg-black ${pokemonInfoBox}`}>
+                    <p className="text-white">Base experience: {base_experience} </p>
+                </div>
+                <div className={`bg-gray-100 ${pokemonInfoBox}`}>
+                    <p>Order: {order} </p>
+                </div>
+            </div>
+            <div className="flex flex-row space-x-4">
+                <PokemonAbility abilities={abilities} />
+                <PokemonMove moves={moves} />
+                <PokemonStat stats={stats} />
+            </div>
+        </div>
+    );
+};
+
+const PokemonAbility: React.FC<{ abilities: any[] }> = ({ abilities }) => (
+    <div className={`bg-black ${pokemonInfoBox}`}>
+        <h3 className="text-lg text-white font-semibold mb-2">Abilities:</h3>
+        <ul>
+            {abilities.map((ability: any) => (
+                <li className="text-white" key={ability.ability.name}>{ability.ability.name}</li>
+            ))}
+        </ul>
+    </div>
+);
+
+
+const PokemonMove: React.FC<{ moves: any[] }> = ({ moves }) => (
+    <div className={`bg-gray-100 ${pokemonInfoBox}`}>
+        <h3 className="text-lg font-semibold mb-2">Move :</h3>
+        <ul>
+            {moves.slice(0, 4).map((move: any) => (
+                <li key={move.move.id}>{move.move.name}</li>
+            ))}
+        </ul>
+    </div>
+);
+
+const PokemonStat: React.FC<{ stats: any[] }> = ({ stats }) => (
+    <div className={`bg-black ${pokemonInfoBox}`}>
+        <h3 className="text-lg text-white font-semibold mb-2">Stat :</h3>
+        <ul>
+            {stats.slice(0, 4).map((stat: any) => (
+                <li className="text-white" key={stat.stat.id}>{stat.stat.name}</li>
+            ))}
+        </ul>
+    </div>
+);
